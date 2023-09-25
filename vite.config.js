331b5e1917f9ebbeb04df12129f3d367aa251d32
Ehsan pirnazar <ehsanre100@gmail.com>
2023-09-25 19:57:39 +0330

    progress: deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/User-panel-v.1/",
   resolve: {
     alias: {
       src: "/src",
